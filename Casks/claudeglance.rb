e3ec08e643447f390ce5bb03a7ce2fe7bc33d0c7
broots144 <workshop144hq@gmail.com>
2026-06-10 18:39:44 -0400

    Update claudeglance to 1.1.4

--- Casks/claudeglance.rb
+++ Casks/claudeglance.rb
@@ -1,6 +1,6 @@
 cask "claudeglance" do
-  version "1.0.0"
-  sha256 "8f6f28b6bedcb475f6e349c3c0cbb4f686941668c1ce2c298342175866af163c"
+  version "1.1.4"
+  sha256 "1b3d17f1529044e20fda4b665ed76cbc0e84f1a930e201f84d60f7d01d71a9fa"
 
   url "https://github.com/broots144/claudeglance/releases/download/v#{version}/ClaudeGlance.dmg"
   name "ClaudeGlance"
@@ -8,6 +8,13 @@ cask "claudeglance" do
   homepage "https://github.com/broots144/claudeglance"
 
   app "ClaudeGlance.app"
+
+  caveats <<~EOS
+    ClaudeGlance is ad-hoc signed (not notarized). On first launch macOS
+    may block it. Right-click the app in /Applications and choose Open,
+    or run:
+      xattr -dr com.apple.quarantine /Applications/ClaudeGlance.app
+  EOS
 
   zap trash: [
     "~/Library/Preferences/io.github.broots144.ClaudeGlance.plist",
