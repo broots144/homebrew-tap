5f1fd490dcee71344ce149b24ccb55df592f1750
broots144 <workshop144hq@gmail.com>
2026-06-10 01:12:15 -0400

    Update claude-usage-systray to 1.1.1

--- Casks/claude-usage-systray.rb
+++ Casks/claude-usage-systray.rb
@@ -1,6 +1,6 @@
 cask "claude-usage-systray" do
-  version "1.1.0"
-  sha256 "31cf438004753b3c297082600cf75e29b8c5bb52b646532a17586958f3ed767b"
+  version "1.1.1"
+  sha256 "036746f515be9d80e5457491693cc76c8aa5070f1fad88b09858c03381f8247c"
 
   url "https://github.com/broots144/claude-usage-systray/releases/download/v#{version}/ClaudeUsageSystray.dmg"
   name "Claude Usage Systray"
